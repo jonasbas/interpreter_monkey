6453bd3cb227cdefb070f7fd8f81289e502211d5
Jonas Bastisch <jonas.bastisch@gmail.com>
2023-04-24 12:36:03 +0200

    add functionality to read literals and numbers

--- src/lexer.rs
+++ src/lexer.rs
@@ -37,9 +37,32 @@ impl Lexer {
         self.read_position += 1;
     }
 
+    fn read_identifier(&mut self) -> Token {
+        let position = self.position;
+        while is_letter(self.ch) {
+            self.read_char();
+        }
+
+        let literal = &self.input[position..self.position];
+
+        let token_type = match literal {
+            "fn" => TokenType::FUNCTION,
+            "let" => TokenType::LET,
+            _ => TokenType::IDENT,
+        };
+
+        Token {
+            token_type,
+            literal: literal.to_string(),
+        }
+    }
+
     fn next_token(&mut self) -> Token {
-        let char_string = self.ch.to_string();
-        let token = match self.ch {
+        self.skip_whitespaces();
+
+        let token_char = self.ch;
+        let char_string = token_char.to_string();
+        let token = match token_char {
             '=' => Token {
                 token_type: TokenType::ASSIGN,
                 literal: char_string,
@@ -76,16 +99,50 @@ impl Lexer {
                 token_type: TokenType::EOF,
                 literal: "".to_string(),
             },
-            _ => Token {
-                token_type: TokenType::EOF,
-                literal: "".to_string(),
-            },
+            _ => {
+                if is_letter(token_char) {
+                    return self.read_identifier();
+                } else if is_digit(token_char) {
+                    return Token {
+                        token_type: TokenType::INT,
+                        literal: self.read_number(),
+                    };
+                } else {
+                    return Token {
+                        token_type: TokenType::ILLEGAL,
+                        literal: char_string,
+                    };
+                }
+            }
         };
 
         self.read_char();
 
         token
     }
+
+    fn read_number(&mut self) -> String {
+        let position = self.position;
+        while is_digit(self.ch) {
+            self.read_char();
+        }
+
+        self.input[position..self.position].to_string()
+    }
+
+    fn skip_whitespaces(&mut self) {
+        while self.ch == ' ' || self.ch == '\t' || self.ch == '\n' || self.ch == '\r' {
+            self.read_char();
+        }
+    }
+}
+
+fn is_letter(ch: char) -> bool {
+    ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z') || ch == '_'
+}
+
+fn is_digit(ch: char) -> bool {
+    '0' <= ch && ch <= '9' && ch != ';'
 }
 
 #[cfg(test)]
@@ -112,6 +169,65 @@ mod tests {
         for x in expected.iter() {
             let token = lexer.next_token();
             assert_eq!(&token.token_type, x);
+        }
+    }
+
+    #[test]
+    fn bigger_token_test() {
+        let string_to_parse = "let five = 5;
+        let ten = 10;
+        let add = fn(x, y) {
+            x + y;
+        };
+        
+        let result = add(five, ten);";
+
+        let expected = vec![
+            (TokenType::LET, "let"),
+            (TokenType::IDENT, "five"),
+            (TokenType::ASSIGN, "="),
+            (TokenType::INT, "5"),
+            (TokenType::SEMICOLON, ";"),
+            (TokenType::LET, "let"),
+            (TokenType::IDENT, "ten"),
+            (TokenType::ASSIGN, "="),
+            (TokenType::INT, "10"),
+            (TokenType::SEMICOLON, ";"),
+            (TokenType::LET, "let"),
+            (TokenType::IDENT, "add"),
+            (TokenType::ASSIGN, "="),
+            (TokenType::FUNCTION, "fn"),
+            (TokenType::LPAREN, "("),
+            (TokenType::IDENT, "x"),
+            (TokenType::COMMA, ","),
+            (TokenType::IDENT, "y"),
+            (TokenType::RPAREN, ")"),
+            (TokenType::LBRACE, "{"),
+            (TokenType::IDENT, "x"),
+            (TokenType::PLUS, "+"),
+            (TokenType::IDENT, "y"),
+            (TokenType::SEMICOLON, ";"),
+            (TokenType::RBRACE, "}"),
+            (TokenType::SEMICOLON, ";"),
+            (TokenType::LET, "let"),
+            (TokenType::IDENT, "result"),
+            (TokenType::ASSIGN, "="),
+            (TokenType::IDENT, "add"),
+            (TokenType::LPAREN, "("),
+            (TokenType::IDENT, "five"),
+            (TokenType::COMMA, ","),
+            (TokenType::IDENT, "ten"),
+            (TokenType::RPAREN, ")"),
+            (TokenType::SEMICOLON, ";"),
+            (TokenType::EOF, ""),
+        ];
+
+        let mut lexer = Lexer::new(string_to_parse);
+
+        for x in expected.iter() {
+            let token = lexer.next_token();
+            println!("{:?}", token);
+            assert_eq!(x, &(token.token_type, token.literal.as_str()))
         }
     }
 }
